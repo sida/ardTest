1c0b787a1f6808810f893d9a0fad28fff68c886e
ysida <sida.yasuharu@gmail.com>
2016-08-21 23:31:23 +0900

    ポート修正

--- PressGame/PressGame.ino
+++ PressGame/PressGame.ino
@@ -1,9 +1,16 @@
 
-int sensorPin = 12;    // select the input pin for the potentiometer
-int led1 = 0;
-int led2 = 1;
-int led3 = 2;
-int led4 = 3;
+//                 +-\/-+
+// ADC0 (D 5) PB5 1|    |8 Vcc
+// ADC3 (D 3) PB3 2|    |7 PB2 (D 2) ADC1
+// ADC2 (D 4) PB4 3|    |6 PB1 (D 1) PWM1
+//            GND 4|    |5 PB0 (D 0) PWM0
+//                 +----+
+
+int sensorPin = 3;    // select the input pin for the potentiometer
+int led1 = PB0;
+int led2 = PB1;
+int led3 = PB2;
+int led4 = PB3;
 
 void setup() {
   pinMode(led1, OUTPUT);
