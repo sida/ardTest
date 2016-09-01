f81f60831f44c143a0027b9a3275b383362d461c
ysida <sida.yasuharu@gmail.com>
2016-09-02 01:52:37 +0900

    コード整理

--- LCD48x128/LCD48x128.ino
+++ LCD48x128/LCD48x128.ino
@@ -16,6 +16,17 @@
   7 GND     ->   GND
 ***/
 
+
+/*
+ * LCDのメモリマップ構造
+ * 
+ * 縦8ドットが横に128個並んだものがページ
+ * 縦8ドットをカラムと呼ぶ。128カラム。
+ * ページはさらに縦に6ページあり縦8*6の46ドットになる
+ * 
+ * 1回書き込みをするとカラムのアドレスがインクリメントされる
+ */
+
 #define RS_PIN  9
 #define CS_PIN 10
 
@@ -37,9 +48,17 @@ void LcdData(uint8_t dat)
 
 void InitLcd()
 {
+  pinMode(RS_PIN, OUTPUT);
+  pinMode(CS_PIN, OUTPUT);
+  digitalWrite(CS_PIN, HIGH);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV32);
+  SPI.setDataMode(SPI_MODE3);
+  SPI.setBitOrder(MSBFIRST);
+
   // http://akizukidenshi.com/download/ds/xiamen/AQM1248.pdf
   // Display OFF
-  LcdCommand(0xae);
+  LcdDisplay(false);
   // ADC = normal
   LcdCommand(0xa0);
   // Common output = revers
@@ -57,9 +76,7 @@ void InitLcd()
   LcdCommand(0x2f);
 
   // コントラスト
-  LcdCommand(0x23);  // Vo voltage resistor ratio set
-  LcdCommand(0x81);  // Electronic volume mode set
-  LcdCommand(0x1c);  // Electronic volume value set
+  LcdContrast(0x1c);
 
   // 表示設定
   // display all point = normal(全点灯しない)
@@ -69,40 +86,81 @@ void InitLcd()
   // display normal/revers = normal (白黒反転しない)
   LcdCommand(0xa6);
   // display = ON
-  LcdCommand(0xaf);
+  LcdDisplay(true);
+}
+
+
+void LcdDisplay(bool on)
+{
+  uint8_t cmd = 0xAE;
+  if (on){
+    cmd |= 1;
+  }
+  LcdCommand(cmd);
+}
+
+// コントラスト
+// value 下位6bit有効 
+void LcdContrast(uint8_t value)
+{
+  LcdCommand(0x23);  // Vo voltage resistor ratio set
+  LcdCommand(0x81);  // Electronic volume mode set
+  LcdCommand(value & 0x3f);  // Electronic volume value set
+}
+
+// ページアドレスの設定　0-7 6,7は表示されない
+void LcdSetPageAdress(uint8_t page)
+{
+  page &= 0x07;
+  LcdCommand(0xb0 | page);
+}
+
+// カラムアドレスの設定 0-131 表示出来るのは0-127
+void LcdSetColumnAdress(uint8_t column)
+{
+  uint8_t upper = column >> 4;
+  uint8_t lower = column & 0x0f;
+  LcdCommand(0x10 | upper);
+  LcdCommand(lower);
 }
 
 void setup() {
-  pinMode(RS_PIN, OUTPUT);
-  pinMode(CS_PIN, OUTPUT);
-  digitalWrite(CS_PIN, HIGH);
-  SPI.begin();
-  SPI.setClockDivider(SPI_CLOCK_DIV32);
-  SPI.setDataMode(SPI_MODE3);
-  SPI.setBitOrder(MSBFIRST);
+
+//  Serial.begin(9600);  
   InitLcd();
+  LcdContrast(0x20);
 }
 
 void loop() {
+//  Serial.println("start");
+
   for (int page = 0; page < 6; page++) {
-    LcdCommand(0xb0 + page);
-    LcdCommand(0x10);
-    LcdCommand(0x00);
+    LcdSetPageAdress(page);
+    LcdSetColumnAdress(0);
+
+    uint8_t val = 0;
+    if (page & 0x01){
+      val = 0xff;
+    }
     for (int x = 0; x < 128; x++) {
-      LcdData(0);
-    } // for x
-  } // for page
+      LcdData(val);
+    }
+  }
 
   delay(1000);
 
   for (int page = 0; page < 6; page++) {
-    LcdCommand(0xb0 + page);
-    LcdCommand(0x10);
-    LcdCommand(0x00);
+    LcdSetPageAdress(page);
+    LcdSetColumnAdress(0);
+
+    uint8_t val = 0x0f;
+//    if (page & 0x01){
+//      val = 0xf0;
+//    }
     for (int x = 0; x < 128; x++) {
-      LcdData(x);
-    } // for x
-  } // for page
+      LcdData(val);
+    }
+  }
 
   delay(1000);
 }
