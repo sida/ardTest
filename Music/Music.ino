58603e1fbacbffba21dbbd7c5a3e7c4d29cd45ab
ysida <sida.yasuharu@gmail.com>
2016-09-11 00:04:21 +0900

    容量削減

--- Music/Music.ino
+++ Music/Music.ino
@@ -22,17 +22,19 @@ This example code is in the public domain.
 #include "melody2.h"
 
 //#define led 3
-#define spk 4
+#define OFF_SWITCH 3
+#define SPEAKER 4
 
 void setup() {
-//  pinMode(led,OUTPUT);
+  pinMode(OFF_SWITCH,INPUT);
 }
 
 void loop() {
   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < (sizeof(noteDurations)>>1); thisNote++) {
-    unsigned int noteDuration = pgm_read_word_near(noteDurations+thisNote);
-    tone(spk, pgm_read_word_near(melody+thisNote));
+  for (int thisNote = 0; thisNote < (sizeof(noteDurations)); thisNote++) {
+    unsigned int noteDuration = pgm_read_byte_near(noteDurations + thisNote);
+    noteDuration = noteDuration * 128;
+    tone(SPEAKER, pgm_read_word_near(melody + thisNote));
 //    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
 
     // to distinguish the notes, set a minimum time between them.
@@ -40,7 +42,7 @@ void loop() {
     delay(noteDuration);
 
     // stop the tone playing:
-    noTone(spk);
+    noTone(SPEAKER);
 //   digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
 //   delay(noteDuration>>2);
    delay(64);
--- Music/melody2.h
+++ Music/melody2.h
@@ -1,87 +1,6 @@
 // We Wish You a Merry Christmas
 // http://piano.score.sukkiri.org/xmas/wish.html
 
-/*
-kore dame
-PROGMEM const unsigned int melody[] = {
-  NOTE_C5, NOTE_F5, NOTE_F5, NOTE_G5, NOTE_F5, NOTE_E5,
-  NOTE_D5, NOTE_D5, NOTE_D5, NOTE_G5,
-  NOTE_G5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_C5,
-  NOTE_C5, NOTE_A5, NOTE_A5, NOTE_C4, NOTE_A5, NOTE_G5, NOTE_F5,
-  NOTE_D5, NOTE_C5, NOTE_C5, NOTE_D5, NOTE_G5,
-  NOTE_E5, NOTE_F5, NOTE_C5,
-  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_E5, NOTE_E5,
-  NOTE_F5, NOTE_E5, NOTE_D5, NOTE_C5, NOTE_G5,
-  NOTE_A5, NOTE_G5, NOTE_F5, NOTE_C5,
-  NOTE_C6, NOTE_C5, NOTE_C5, NOTE_D5, NOTE_G5, NOTE_E5, NOTE_F5
-};
-
-PROGMEM const unsigned int noteDurations[] = {
-  256, 256, 128, 128, 128, 128,
-  256, 256, 256, 256,
-  128, 128, 128, 128, 256, 256,
-  256, 256, 128, 128, 128, 128, 256,
-  256, 128, 128, 256, 256,
-  256, 512, 256,
-  256, 256, 256, 512, 256,
-  256, 256, 256, 512, 256,
-  256, 256, 256, 256,
-  256, 128, 128, 256, 256, 256, 512
-};
-*/
-
-/*
-PROGMEM const unsigned int melody[] = {
-  NOTE_D5,  NOTE_G5,
-  NOTE_G5,  NOTE_A5,  NOTE_G5,  NOTE_F5,  NOTE_E5,  NOTE_E5, NOTE_RT
-  NOTE_E5,  NOTE_A5,  NOTE_A5,  NOTE_B5,  NOTE_A5,  NOTE_G5,
-  NOTE_F5,  NOTE_B5,  NOTE_RT,  NOTE_B5,  NOTE_B5,
-  NOTE_B5,  NOTE_C6,  NOTE_B5,  NOTE_A5,  NOTE_G5,  NOTE_E5,
-  NOTE_D5,  NOTE_D5,  NOTE_E5,  NOTE_A5,  NOTE_F5
-};
-
-PROGMEM const unsigned int noteDurations[] = {
- 256, 256,
- 128, 128, 128, 128, 256, 128, 128,
- 256, 256, 128, 128, 128, 128,
- 256, 128, 128, 256, 256,
- 128, 128, 128, 128, 256, 256,
- 128, 128, 256, 256, 256
-};
-
-*/
-
-/*
-PROGMEM const unsigned int melody[] = {
-  NOTE_D5,  NOTE_D5,  NOTE_E5,  NOTE_A5,  NOTE_FS5 , NOTE_G5
-//  NOTE_C5,  NOTE_C5,  NOTE_D5,  NOTE_G5,  NOTE_E5, NOTE_F5
-};
-
-PROGMEM const unsigned int noteDurations[] = {
- 128, 128, 256, 256, 256 ,512
-};
-*/
-
-/*
-PROGMEM const unsigned int melody[] = {
-  NOTE_D5,  NOTE_G5,
-  NOTE_G5,  NOTE_A5,  NOTE_G5,  NOTE_F5,  NOTE_E5,  NOTE_E5, NOTE_RT,
-  NOTE_E5,  NOTE_A5,  NOTE_A5,  NOTE_B5,  NOTE_A5,  NOTE_G5,
-  NOTE_F5,  NOTE_B5,  NOTE_RT,  NOTE_B5,  NOTE_B5,
-  NOTE_B5,  NOTE_C6,  NOTE_B5,  NOTE_A5,  NOTE_G5,  NOTE_E5,
-  NOTE_D5,  NOTE_D5,  NOTE_E5,  NOTE_A5,  NOTE_FS5, NOTE_G5
-};
-
-PROGMEM const unsigned int noteDurations[] = {
- 256, 256,
- 128, 128, 128, 128, 256, 128, 128,
- 256, 256, 128, 128, 128, 128,
- 256, 128, 128, 256, 256,
- 128, 128, 128, 128, 256, 256,
- 128, 128, 256, 256, 256, 512
-};
-*/
-
 PROGMEM const unsigned int melody[] = {
   NOTE_D5,NOTE_G5,
   NOTE_G5,NOTE_A5,NOTE_G5,NOTE_FS5,NOTE_E5,NOTE_E5,NOTE_RT,
@@ -106,26 +25,27 @@ PROGMEM const unsigned int melody[] = {
   NOTE_G5
 };
 
-PROGMEM const unsigned int noteDurations[] = {
-  256,256,
-  128,128,128,128,256,128,128,
-  256,256,128,128,128,128,
-  256,128,128,256,256,
-  128,128,128,128,256,256,
-  128,128,256,256,256,
+PROGMEM const unsigned char noteDurations[] = {
+  2,2,
+  1,1,1,1,2,1,1,
+  2,2,1,1,1,1,
+  2,1,1,2,2,
+  1,1,1,1,2,2,
+  1,1,2,2,2,
 
-  394,128,256,256,
-  256,256,512,
-  256,256,256,256,
-  394,128,256,256,
-  256,256,256,256,
-  128,128,256,256,256,
-  394,128,256,256,
+  3,1,2,2,
+  2,2,4,
+  2,2,2,2,
+  3,1,2,2,
+  2,2,2,2,
+  1,1,2,2,2,
+  3,1,2,2,
 
-  128,128,128,128,256,128,128,
-  256,256,128,128,128,128,
-  256,128,128,256,256,
-  128,128,128,128,256,256,
-  128,128,256,256,256,
-  512
+  1,1,1,1,2,1,1,
+  2,2,1,1,1,1,
+  2,1,1,2,2,
+  1,1,1,1,2,2,
+  1,1,2,2,2,
+  4
 };
+
